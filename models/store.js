0b33ef5f2b4644b60717d7d6a02ae3b21242fa52
rajeshmn47 <rajeshmn47@gmail.com>
2022-04-06 10:21:59 +0530

    added location

--- models/store.js
+++ models/store.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 var storeSchema =  new Schema({
-    name:String,
     title:String,
     img_url:String,
     maincategory:String,
